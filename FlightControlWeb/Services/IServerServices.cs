756dccf4d2e84ac18643d407070351a56befe342
Guy Shoham <guyshoham28@gmail.com>
2020-05-01 17:10:17 +0300

    Added Server class, controller and service

---
+++ FlightControlWeb/Services/IServerServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlightControlWeb.Models;
+
+
+namespace FlightControlWeb.Services
+{
+    public interface IServerServices
+    {
+        ServerItems AddServerItems(ServerItems items);
+        Dictionary<string, ServerItems> GetServerItems();
+        ServerItems DeleteServerItems(string id);
+    }
+}
