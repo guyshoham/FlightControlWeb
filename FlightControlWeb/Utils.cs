7be31eb6882282e01acda823bdd9afdc6b1437f0
Guy Shoham <guyshoham28@gmail.com>
2020-05-03 16:01:50 +0300

    implement logic behind GetAllFlightsRelativeToDate (didn't tested) (#12, #13)

--- FlightControlWeb/Utils.cs
+++ FlightControlWeb/Utils.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using FlightControlWeb.Models;
 
 namespace FlightControlWeb
 {
@@ -24,6 +26,93 @@ namespace FlightControlWeb
             string dig6 = (rand.Next(0, 10)).ToString(); // random digit [0-9]
 
             return letter + dig1 + dig2 + dig3 + '-' + dig4 + dig5 + dig6;
+        }
+
+        public static List<Flight> GetAllFlightsRelativeToDate(Dictionary<string, FlightPlan> plans, DateTime dateInput)
+        {
+            List<Flight> list = new List<Flight>();
+
+            // iterate over all plans in dictionary
+            foreach (FlightPlan plan in plans.Values)
+            {
+                // get flight from this plan (if not fit, returns null)
+                Flight f = GetFlight(plan, dateInput);
+                if (f != null)
+                {
+                    list.Add(f);
+                }
+            }
+
+            return list;
+        }
+
+        public static Flight GetFlight(FlightPlan plan, DateTime dateInput)
+        {
+            DateTime departureTime = plan.InitialLocation.DateTime;
+            if (DateTime.Compare(departureTime, dateInput) > 0)
+            {
+                // dateInput is before the flight departure, skip this flight
+                return null;
+            }
+
+            // check segments one by one until reach time (or not)
+            DateTime currTime = departureTime;
+            Segment prevSegment = new Segment
+            {
+                Longitude = plan.InitialLocation.Longitude,
+                Latitude = plan.InitialLocation.Latitude
+            };
+
+            // iterate over all segments in plan
+            foreach (Segment s in plan.Segments)
+            {
+                currTime = currTime.AddSeconds(s.TimespanSeconds);
+                if (DateTime.Compare(currTime, dateInput) > 0)
+                {
+                    // we need to calculate location in this segment
+                    int time = currTime.Second - dateInput.Second;
+                    Segment retVal = GetLocation(prevSegment, s, time);
+
+                    // create Flight object
+                    Flight flight = new Flight
+                    {
+                        FlightId = plan.FlightPlanId,
+                        Longitude = retVal.Longitude,
+                        Latitude = retVal.Latitude,
+                        Passengers = plan.Passengers,
+                        CompanyName = plan.CompanyName,
+                        DateTime = dateInput,
+                        IsExternal = false // TODO: what value does it get and from where?
+                    };
+
+
+                    return flight;
+                }
+                prevSegment = s;
+            }
+
+            // dateInput is after the flight landing, skip this flight
+            return null;
+        }
+
+        public static Segment GetLocation(Segment pointA, Segment pointB, int duration)
+        {
+            //get pace of lat/long relative to one second
+            double paceLat = (pointB.Latitude - pointA.Latitude) / pointB.TimespanSeconds;
+            double paceLong = (pointB.Longitude - pointA.Longitude) / pointB.TimespanSeconds;
+
+            //get lat/long for the disire location
+            double latitude = pointA.Latitude + paceLat * duration;
+            double longitude = pointA.Longitude + paceLong * duration;
+
+            // create new Segment with the lat/long result
+            Segment location = new Segment
+            {
+                Latitude = latitude,
+                Longitude = longitude
+            };
+
+            return location;
         }
     }
 }
