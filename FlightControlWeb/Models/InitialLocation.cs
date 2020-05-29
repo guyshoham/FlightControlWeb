a56e3e353eee0ddd31d92d1b24c59eaa4d751c31
Guy Shoham <guyshoham28@gmail.com>
2020-05-29 12:30:35 +0300

    trying to fix bug

--- FlightControlWeb/Models/InitialLocation.cs
+++ FlightControlWeb/Models/InitialLocation.cs
@@ -1,14 +1,17 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FlightControlWeb.Models
 {
     public class InitialLocation
     {
         [JsonProperty(PropertyName = "longitude")]
+        [Range(-180, 180, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public double Longitude { get; set; }
 
         [JsonProperty(PropertyName = "latitude")]
+        [Range(-90, 90, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public double Latitude { get; set; }
 
         [JsonProperty(PropertyName = "date_time")]
